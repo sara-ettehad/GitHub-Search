d75bc15e96446ca16bd3f0e3fb16234c6c1df67f
Parisa Ettehad <92958591+parisa-ettd@users.noreply.github.com>
2022-06-11 18:06:17 -0400

    main

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ const locationEl = document.getElementById("profile-location");
             repoEl.textContent = profiles.public_repos;
             
 
-            locationEl.textContent = `📍${profiles.location}` ;
+            locationEl.textContent = `🌍 ${profiles.location}` ;
             profiles.location
             ? locationEl.parentElement.classList.remove("p-none")
             : locationEl.parentElement.classList.add("p-none");
